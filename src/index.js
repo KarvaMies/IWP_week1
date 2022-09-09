8873334f693506421a6a6f54dc02b051b7b44b6f
KarvaMies <80526519+KarvaMies@users.noreply.github.com>
2022-09-09 15:06:26 +0300

    task 4 commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ document.getElementById("app").innerHTML = `
 
 const printButton = document.getElementById("my-button");
 const header = document.getElementById("header");
+const addDataButton = document.getElementById("add-data");
+
 printButton.addEventListener("click", function () {
   console.log("hello world");
   header.innerText = "My notebook";
+});
+
+addDataButton.addEventListener("click", function () {
+  const list = document.getElementById("data-list");
+  let newListItem = document.createElement("li");
+  newListItem.innerText = "This is Data";
+  list.appendChild(newListItem);
 });
